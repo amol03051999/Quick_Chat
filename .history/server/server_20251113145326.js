a4191e0a3e89c3e5692315470626dab1e7748988
amol shinde <amolsh249@gmail.com>
2025-11-13 14:57:30 +0530

    deploy changes

--- .history/server/server_20251113145326.js
+++ .history/server/server_20251113145326.js
@@ -51,7 +51,10 @@ app.use('/api/messages',messageRouter)
 //connect to mongoDB
 await connectDB();
 
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log('server running on PORT:' + PORT));
 
+}
 
+expoe
